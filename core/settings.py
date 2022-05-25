2809a553d31e0ea0c383e9b5db5986ac967274e1
ThiagoBmg <th.bmg5274@gmail.com>
2022-05-25 19:01:35 -0300

    ajustando

--- core/settings.py
+++ core/settings.py
@@ -116,12 +116,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'statics/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+import os 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Search for additional static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static '),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
